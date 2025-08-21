625bf69f438e6e7d63b6a9de982f1752668c1f16
elizabethllazarenko-collab <elizabethllazarenko@gmail.com>
2025-08-21 18:50:36 +0300

    My homework for lesson 3, part 2

---
+++ homework3.3.py
@@ -0,0 +1,11 @@
+def split_list(lst):
+    n = len(lst)
+    mid = (n + 1) // 2
+    first = lst[:mid]
+    second = lst[mid:]
+    return [first, second]
+print(split_list([1, 2, 3, 4, 5, 6]))
+print(split_list([1, 2, 3]))
+print(split_list([1, 2, 3, 4, 5]))
+print(split_list([1]))
+print(split_list([]))
